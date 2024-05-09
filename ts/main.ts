5796a20c03adf0dd3604a3caae41accc86171ee0
LunarSeaCatFish <DedicatedGeneric35711@outlook.com>
2024-05-09 15:10:01 -0700

    Removed unneeded test code.

--- ts/main.ts
+++ ts/main.ts
@@ -24,14 +24,6 @@ class Book {
     releaseDate : Date;
 }
 
-// Book object test code
-let myBook = new Book();
-myBook.isbn = "123";
-myBook.price = 9.99;
-myBook.title = "Programming for beginners";
-myBook.releaseDate = new Date(2023, 9, 8); // Months start at index 0
-
-console.log(myBook);
 
 
 window.onload = function () {
